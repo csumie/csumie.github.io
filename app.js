08f3ac098d855a7c4ce6eaf6b01bc51e5189b54e
csumie <csurdanivia@gmail.com>
2016-06-27 19:30:34 -0400

    made changes to index.html and app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 alert("Hi Planetoid!");
+
+$(document).ready(function(){
+    $("select.country").change(function(){
+        var selectedCountry = $(".country option:selected").val();
+        $.ajax({
+            type: "POST",
+            url: "../php/process-request.php",
+            data: { country : selectedCountry } 
+        }).done(function(data){
+            $("#response").html(data);
+        });
+    });
+});
